cffc5bd69eb0a1f84094a7c4501e6f2a3345b466
mori8 <kaithape@gmail.com>
2020-08-25 01:45:06 +0900

    dynamically generate svg file

--- api/views.py
+++ api/views.py
@@ -44,21 +44,67 @@ def generate_badge(request):
 
     user_information_url = api_server + '/user_information.php?id=' + boj_handle
     json = requests.get(user_information_url).json()
-    tier = TIERS[json['level']]
+    level = json['level']
 
-    svg = """
+    tier, limit, *rest = [thres for thres in threshold if level <= thres]
+    tier_title, tier_rank = TIERS[level].split()    
+    
+    svg = '''
     <!DOCTYPE svg PUBLIC 
         "-//W3C//DTD SVG 1.1//EN" 
         "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-    <svg height="60" width="200"   
-        version="1.1" 
-        xmlns="http://www.w3.org/2000/svg" 
-        xmlns:xlink="http://www.w3.org/1999/xlink" 
-        xml:space="preserve">
-       <text x="0" y="15" fill="red" transform="rotate(30 20,40)">boj : {boj_handle}</text>
-       <text x="0" y="15" fill="red" transform="rotate(30 20,40)">tier : {tier}</text>
-    </svg>
-    """.format(boj_handle=boj_handle, tier=tier)
+<svg height="170" width="350"   
+    version="1.1" 
+    xmlns="http://www.w3.org/2000/svg" 
+    xmlns:xlink="http://www.w3.org/1999/xlink" 
+    xml:space="preserve">
+    <style type="text/css">
+        <![CDATA[
+        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&display=swap');
+        :root {{
+            background-image: linear-gradient(30deg, rgb(140, 197, 132) 40%, rgb(69, 178, 211) 100%, rgb(81, 167, 149) 40%);
+            background-repeat: no-repeat;
+        }}
+        text {{
+            fill: white;
+            font-family: 'Noto Sans KR', sans-serif;
+        }}
+        text.boj-handle {{
+            font-weight: 700;
+            font-size: 1.5em;
+        }}
+        text.tier-text {{
+            font-family: 'Caveat', cursive;
+            font-size: 2em;
+        }}
+        text.tier-number {{
+            font-size: 3.3em;
+            font-weight: 700;
+        }}
+        .subtitle {{
+            font-weight: 300;
+            font-size: 0.9em;
+        }}
+        .value {{
+            font-weight: 400;
+        }}
+        ]]>
+    </style>
+    <line x1="30" y1="60" x2="30" y2="120" stroke-width="2" stroke="white"/>
+    <line x1="100" y1="60" x2="100" y2="120" stroke-width="2" stroke="white"/>
+    <line x1="30" y1="120" x2="65" y2="140" stroke-width="2" stroke="white"/>
+    <line x1="100" y1="120" x2="65" y2="140" stroke-width="2" stroke="white"/>
+    <line x1="30" y1="128" x2="65" y2="148" stroke-width="2" stroke="white"/>
+    <line x1="100" y1="128" x2="65" y2="148" stroke-width="2" stroke="white"/>
+    <text x="145" y="60" class="boj-handle">{boj_handle}</text>
+    <text transform="translate(62, 48)" text-anchor="middle" alignment-baseline="middle" class="tier-text">{tier_title}</text>
+    <text x="50" y="115" class="tier-number">{tier_rank}</text>
+    <text x="145" y="95" class="subtitle">solved</text><text x="260" y="95" class="solved value">291</text>
+    <text x="145" y="115" class="subtitle">class</text><text x="260" y="115" class="class value">3</text>
+    <text x="145" y="135" class="subtitle">something</text><text x="260" y="135" class="something value">merong</text>
+</svg>
+    '''.format(boj_handle=boj_handle, tier_rank=tier_rank, tier_title=tier_title)
 
     response = HttpResponse(content=svg)
     response['Content-Type'] = 'image/svg+xml'
