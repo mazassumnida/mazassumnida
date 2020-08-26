7d32be4277b83eda2a7cada1ed0e3ec238e7f33f
strawji <strawji02@naver.com>
2020-08-26 16:25:40 +0900

    views.py - try 후 KeyError 나오면 Unknown용 이미지 제작(404page)

--- api/views.py
+++ api/views.py
@@ -7,6 +7,8 @@ from django.http import HttpResponse
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 # Create your views here.
+
+
 def generate_badge(request):
     TIERS = (
         "Unrated",
@@ -41,7 +43,7 @@ def generate_badge(request):
         "Ruby 2",
         "Ruby 1",
     )
-    
+
     BACKGROUND_COLOR = {
         'Unrated': ['#666666', '#2D2D2D', '#030202'],
         'Bronze': ['#F49347', '#984400', '#6E3100'],
@@ -56,111 +58,201 @@ def generate_badge(request):
     boj_handle = request.GET.get("boj", "ccoco")
 
     user_information_url = api_server + '/v2/users/show.json?id=' + boj_handle
-    json = requests.get(user_information_url).json()
-    json = json["result"]['user'][0]
-    level = json['level']
-    solved = '{0:n}'.format(json['solved'])
-    boj_class = json['class']
-    
-    next_exp = json['next_exp_cap']
-    prev_exp = json['previous_exp_cap']
-    exp_gap = next_exp - prev_exp
-    my_exp = json['exp']
-    percentage = round((my_exp - prev_exp) * 100 / exp_gap)
-    bar_size = 35 + 2.55 * percentage
-    
-    needed_exp = '{0:n}'.format(next_exp - prev_exp)
-    now_exp = '{0:n}'.format(my_exp - prev_exp)
-    exp = '{0:n}'.format(my_exp)
-    
-    if TIERS[level] == 'Unrated':
-        tier_title = TIERS[level]
-        tier_rank = ''
-    else:
-        tier_title, tier_rank = TIERS[level].split()    
-    
-    svg = '''
-    <!DOCTYPE svg PUBLIC 
-        "-//W3C//DTD SVG 1.1//EN" 
-        "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg height="170" width="350"   
-    version="1.1" 
-    xmlns="http://www.w3.org/2000/svg" 
-    xmlns:xlink="http://www.w3.org/1999/xlink" 
-    xml:space="preserve">
-    <style type="text/css">
-        <![CDATA[
-            @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=block');
-            .background {{
-                fill: url(#grad);
-            }}
-            text {{
-                fill: white;
-                font-family: 'Noto Sans KR', sans-serif;
-            }}
-            text.boj-handle {{
+
+    try:
+        json = requests.get(user_information_url).json()
+        json = json["result"]['user'][0]
+        level = json['level']
+        solved = '{0:n}'.format(json['solved'])
+        boj_class = json['class']
+
+        next_exp = json['next_exp_cap']
+        prev_exp = json['previous_exp_cap']
+        exp_gap = next_exp - prev_exp
+        my_exp = json['exp']
+        percentage = round((my_exp - prev_exp) * 100 / exp_gap)
+        bar_size = 35 + 2.55 * percentage
+
+        needed_exp = '{0:n}'.format(next_exp - prev_exp)
+        now_exp = '{0:n}'.format(my_exp - prev_exp)
+        exp = '{0:n}'.format(my_exp)
+
+        if TIERS[level] == 'Unrated':
+            tier_title = TIERS[level]
+            tier_rank = ''
+        else:
+            tier_title, tier_rank = TIERS[level].split()
+
+        svg = '''
+        <!DOCTYPE svg PUBLIC 
+            "-//W3C//DTD SVG 1.1//EN" 
+            "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+    <svg height="170" width="350"   
+        version="1.1" 
+        xmlns="http://www.w3.org/2000/svg" 
+        xmlns:xlink="http://www.w3.org/1999/xlink" 
+        xml:space="preserve">
+        <style type="text/css">
+            <![CDATA[
+                @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=block');
+                .background {{
+                    fill: url(#grad);
+                }}
+                text {{
+                    fill: white;
+                    font-family: 'Noto Sans KR', sans-serif;
+                }}
+                text.boj-handle {{
+                    font-weight: 700;
+                    font-size: 1.45em;
+                }}
+                text.tier-text {{
                 font-weight: 700;
                 font-size: 1.45em;
-            }}
-            text.tier-text {{
-              font-weight: 700;
-              font-size: 1.45em;
-              opacity: 55%;
-            }}
-            text.tier-number {{
-                font-size: 3.1em;
+                opacity: 55%;
+                }}
+                text.tier-number {{
+                    font-size: 3.1em;
+                    font-weight: 700;
+                }}
+                .subtitle {{
+                    font-weight: 500;
+                    font-size: 0.9em;
+                }}
+                .value {{
+                    font-weight: 400;
+                    font-size: 0.9em;
+                }}
+                .percentage {{
+                    font-weight: 300;
+                    font-size: 0.8em;
+                }}
+                .progress {{
+                    font-size: 0.7em;
+                }}
+            ]]>
+        </style>
+        <defs>
+            <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="35%">
+                <stop offset="10%" style="stop-color:{color1};stop-opacity:1" />
+                <stop offset="55%" style="stop-color:{color2};stop-opacity:1" />
+                <stop offset="100%" style="stop-color:{color3};stop-opacity:1" />
+            </linearGradient>
+        </defs>
+        <rect width="350" height="170" rx="10" ry="10" class="background"/>
+        <text x="315" y="50" class="tier-text" text-anchor="end">{tier_title}{tier_rank}</text>
+        <text x="35" y="50" class="boj-handle">{boj_handle}</text>
+        <text x="35" y="78" class="subtitle">class</text><text x="145" y="78" class="class value">{boj_class}</text>
+        <text x="35" y="98" class="subtitle">solved</text><text x="145" y="98" class="solved value">{solved}</text>
+        <text x="35" y="118" class="subtitle">exp</text><text x="145" y="118" class="something value">{exp}</text>
+        <line x1="35" y1="142" x2="{bar_size}" y2="142" stroke-width="4" stroke="floralwhite" stroke-linecap="round"/>
+        <line x1="35" y1="142" x2="290" y2="142" stroke-width="4" stroke-opacity="40%" stroke="floralwhite" stroke-linecap="round"/>
+        <text x="297" y="142" alignment-baseline="middle" class="percentage">{percentage}%</text>
+        <text x="293" y="157" class="progress" text-anchor="end">{now_exp} / {needed_exp}</text>
+    </svg>
+        '''.format(color1=BACKGROUND_COLOR[tier_title][0],
+                   color2=BACKGROUND_COLOR[tier_title][1],
+                   color3=BACKGROUND_COLOR[tier_title][2],
+                   boj_handle=boj_handle,
+                   tier_rank=tier_rank,
+                   tier_title=tier_title,
+                   solved=solved,
+                   boj_class=boj_class,
+                   exp=exp,
+                   now_exp=now_exp,
+                   needed_exp=needed_exp,
+                   percentage=percentage,
+                   bar_size=bar_size)
+
+        response = HttpResponse(content=svg)
+        response['Content-Type'] = 'image/svg+xml'
+
+        return response
+
+    except:
+        KeyError
+        svg = '''
+        <!DOCTYPE svg PUBLIC 
+            "-//W3C//DTD SVG 1.1//EN" 
+            "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+    <svg height="170" width="350"   
+        version="1.1" 
+        xmlns="http://www.w3.org/2000/svg" 
+        xmlns:xlink="http://www.w3.org/1999/xlink" 
+        xml:space="preserve">
+        <style type="text/css">
+            <![CDATA[
+                @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=block');
+                .background {{
+                    fill: url(#grad);
+                }}
+                text {{
+                    fill: white;
+                    font-family: 'Noto Sans KR', sans-serif;
+                }}
+                text.boj-handle {{
+                    font-weight: 700;
+                    font-size: 1.45em;
+                }}
+                text.tier-text {{
                 font-weight: 700;
-            }}
-            .subtitle {{
-                font-weight: 500;
-                font-size: 0.9em;
-            }}
-            .value {{
-                font-weight: 400;
-                font-size: 0.9em;
-            }}
-            .percentage {{
-                font-weight: 300;
-                font-size: 0.8em;
-            }}
-            .progress {{
-                font-size: 0.7em;
-            }}
-        ]]>
-    </style>
-    <defs>
-        <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="35%">
-            <stop offset="10%" style="stop-color:{color1};stop-opacity:1" />
-            <stop offset="55%" style="stop-color:{color2};stop-opacity:1" />
-            <stop offset="100%" style="stop-color:{color3};stop-opacity:1" />
-        </linearGradient>
-    </defs>
-    <rect width="350" height="170" rx="10" ry="10" class="background"/>
-    <text x="315" y="50" class="tier-text" text-anchor="end">{tier_title}{tier_rank}</text>
-    <text x="35" y="50" class="boj-handle">{boj_handle}</text>
-    <text x="35" y="78" class="subtitle">class</text><text x="145" y="78" class="class value">{boj_class}</text>
-    <text x="35" y="98" class="subtitle">solved</text><text x="145" y="98" class="solved value">{solved}</text>
-    <text x="35" y="118" class="subtitle">exp</text><text x="145" y="118" class="something value">{exp}</text>
-    <line x1="35" y1="142" x2="{bar_size}" y2="142" stroke-width="4" stroke="floralwhite" stroke-linecap="round"/>
-    <line x1="35" y1="142" x2="290" y2="142" stroke-width="4" stroke-opacity="40%" stroke="floralwhite" stroke-linecap="round"/>
-    <text x="297" y="142" alignment-baseline="middle" class="percentage">{percentage}%</text>
-    <text x="293" y="157" class="progress" text-anchor="end">{now_exp} / {needed_exp}</text>
-</svg>
-    '''.format(color1=BACKGROUND_COLOR[tier_title][0],
-               color2=BACKGROUND_COLOR[tier_title][1],
-               color3=BACKGROUND_COLOR[tier_title][2],
-               boj_handle=boj_handle,
-               tier_rank=tier_rank,
-               tier_title=tier_title,
-               solved=solved,
-               boj_class=boj_class,
-               exp=exp,
-               now_exp=now_exp,
-               needed_exp=needed_exp,
-               percentage=percentage,
-               bar_size=bar_size)
+                font-size: 1.45em;
+                opacity: 55%;
+                }}
+                text.tier-number {{
+                    font-size: 3.1em;
+                    font-weight: 700;
+                }}
+                .subtitle {{
+                    font-weight: 500;
+                    font-size: 0.9em;
+                }}
+                .value {{
+                    font-weight: 400;
+                    font-size: 0.9em;
+                }}
+                .percentage {{
+                    font-weight: 300;
+                    font-size: 0.8em;
+                }}
+                .progress {{
+                    font-size: 0.7em;
+                }}
+            ]]>
+        </style>
+        <defs>
+            <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="35%">
+                <stop offset="10%" style="stop-color:{color1};stop-opacity:1" />
+                <stop offset="55%" style="stop-color:{color2};stop-opacity:1" />
+                <stop offset="100%" style="stop-color:{color3};stop-opacity:1" />
+            </linearGradient>
+        </defs>
+        <rect width="350" height="170" rx="10" ry="10" class="background"/>
+        <text x="315" y="50" class="tier-text" text-anchor="end">{tier_title}{tier_rank}</text>
+        <text x="35" y="50" class="boj-handle">{boj_handle}</text>
+        <text x="35" y="78" class="subtitle">class</text><text x="145" y="78" class="class value">{boj_class}</text>
+        <text x="35" y="98" class="subtitle">solved</text><text x="145" y="98" class="solved value">{solved}</text>
+        <text x="35" y="118" class="subtitle">exp</text><text x="145" y="118" class="something value">{exp}</text>
+        <line x1="35" y1="142" x2="{bar_size}" y2="142" stroke-width="4" stroke="floralwhite" stroke-linecap="round"/>
+        <line x1="35" y1="142" x2="290" y2="142" stroke-width="4" stroke-opacity="40%" stroke="floralwhite" stroke-linecap="round"/>
+        <text x="297" y="142" alignment-baseline="middle" class="percentage">{percentage}%</text>
+        <text x="293" y="157" class="progress" text-anchor="end">{now_exp} / {needed_exp}</text>
+    </svg>
+        '''.format(color1='#AAAAAA',
+                   color2='#666666',
+                   color3='#000000',
+                   boj_handle='unknown',
+                   tier_rank='',
+                   tier_title='unknown',
+                   solved='0',
+                   boj_class='0',
+                   exp='0',
+                   now_exp='0',
+                   needed_exp='0',
+                   percentage='0',
+                   bar_size='35')
 
-    response = HttpResponse(content=svg)
-    response['Content-Type'] = 'image/svg+xml'
+        response = HttpResponse(content=svg)
+        response['Content-Type'] = 'image/svg+xml'
 
-    return response
+        return response
